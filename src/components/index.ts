cc9347c7899e2c664261bf3924b848a69ea8a192
mattismyname3011 <mattismyname3011@gmail.com>
2024-08-03 18:54:39 +0800

    new update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Icons from "./global/icons";
+import Container from "./global/container";
+import Wrapper from "./global/wrapper";
+import Navbar from "./navigation/navbar";
 
-export {
-    Icons,
-}
+export { Icons, Navbar, Container, Wrapper };
