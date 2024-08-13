7797b4b6f1fb74080288a2177977e2d776611299
mattismyname3011 <mattismyname3011@gmail.com>
2024-08-13 22:24:01 +0800

    Pricing Section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { Wrapper, Container } from "@/components";
-import { ChevronRight, ArrowRight } from "lucide-react";
+import { ChevronRight, ArrowRight, Zap } from "lucide-react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { BorderBeam } from "@/components/ui/border-beam";
+import { perks, pricingCards } from "@/constants";
+import SectionBadge from "@/components/ui/section-badge";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
 const HomePage = () => {
   return (
-    <section className="w-full relative flex items-center flex-col justify-center px-4 md:px-0 py-8">
+    <section className="w-full relative flex items-center justify-center flex-col px-4 md:px-0 py-8">
+      {/* <section className="w-full relative flex items-center flex-col justify-center px-4 md:px-0 py-8"> */}
       {/* Hero */}
+
       <Wrapper>
+        <div className="absolute inset-0 dark:bg-[linear-gradient(to_right,#e2e8f0_1px,transparent_1px),linear-gradient(to_bottom,#e2e8f0_1px,transparent_1px)] bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10 h-[150vh]" />
+
         <Container>
-          <div className="flex flex-col items-center justify-center py-20 w-full">
+          <div className="flex flex-col items-center justify-center py-20 h-full">
             <button className="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
               <span>
                 <span className="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
@@ -31,38 +47,148 @@ const HomePage = () => {
               </span>
             </button>
 
-            <div className="flex flex-col items-center mt-8 max-w-4xl mx-auto w-11/12 md:w-full">
-              <h1 className="text-4xl md:text-6xl md:!leading-snug font-semibold text-center bg-clip-text text-white">
-                {/* Build your next idea and ship your dream site */}
+            <div className="flex flex-col items-center mt-8 max-w-4xl w-11/12 md:w-full">
+              <h1 className="text-4xl md:text-6xl lg:textxl md:!leading-snug font-semibold text-center bg-clip-text bg-gradient-to-b from-gray-50 to-gray-50 text-transparent">
                 Humanize AI to sounds like us
               </h1>
               <p className="text-base md:text-lg text-foreground/80 mt-6 text-center">
-                {/* Zero code, maximum speed. Make professional sites easy, fast and
-                fun while delivering best-in-class SEO, performance. */}
                 Humanized content, undetectable by AI. Transform AI-generated
                 text into natural-sounding content with ease, and manage your
                 usage with a flexible token system based on your needs.
               </p>
               <div className="hidden md:flex relative items-center justify-center mt-8 md:mt-12 w-full">
-                <div
-                  //   href={`/`}
-                  className="flex items-center justify-center w-max rounded-full border-t border-foreground/30 bg-white/20 backdrop-blur-lg px-2 py-1 md:py-2 md:gap-8 shadow-2xl shadow-foreground/40  select-none"
+                <Link
+                  href="#"
+                  className="flex items-center justify-center w-max rounded-full border-t border-foreground/30 bg-white/20 backdrop-blur-lg px-2 py-1 md:py-2 gap-2 md:gap-8 shadow-3xl shadow-background/40 cursor-pointer select-none"
                 >
-                  <p className="text-foreground text-sm text-center md:text-base font-medium pl-4 pr-4 lg:marker:pr-0">
-                    ✨{""} Humanizing your content right now!
+                  <p className="text-foreground text-sm text-center md:text-base font-medium pl-4 pr-4 lg:pr-0">
+                    ✨ {"  "} Humanizing your content right now!
                   </p>
-                  <Link href={`/`}>
-                    <Button
-                      size={`sm`}
-                      className="rounded-full hidden lg:flex border border-foreground/20"
-                    >
-                      Get Started
-                      <ArrowRight className="w-4 h-4 ml-1" />
-                    </Button>
-                  </Link>
-                </div>
+                  <Button
+                    size="sm"
+                    className="rounded-full hidden lg:flex border border-foreground/20"
+                  >
+                    Get Started
+                    <ArrowRight className="w-4 h-4 ml-1" />
+                  </Button>
+                </Link>
               </div>
             </div>
+
+            <div className="relative flex items-center justify-center py-10 md:py-20 w-full">
+              <div className="absolute top-1/2 left-1/2 -z-10 gradient w-3/4 -translate-x-1/2 h-3/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
+              <div className="-m-2 rounded-xl p-2 ring-1 ring-inset ring-foreground/20 lg:-m-4 lg:rounded-2xl bg-opacity-50 backdrop-blur-3xl">
+                <Image
+                  src="/assets/dashboard.svg"
+                  alt="banner image"
+                  width={1200}
+                  height={1200}
+                  quality={100}
+                  className="rounded-md lg:rounded-xl bg-foreground/10 shadow-2xl ring-1 ring-border"
+                />
+
+                <BorderBeam size={250} duration={12} delay={9} />
+              </div>
+            </div>
+          </div>
+        </Container>
+      </Wrapper>
+
+      <div className="absolute inset-0 dark:bg-[linear-gradient(to_right,#e2e8f0_1px,transparent_1px),linear-gradient(to_bottom,#e2e8f0_1px,transparent_1px)] bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10 h-[150vh]" />
+
+      {/* how it works */}
+      <Wrapper className="flex flex-col items-center justify-center py-12 relative">
+        <Container>
+          <div className="max-w-lg mx-auto text-start md:text-center">
+            <SectionBadge title="The Process" />
+            <h2 className="text-3xl lg:text-4xl font-semibold mt-6">
+              Three steps to humanize your AI-generated content
+            </h2>
+            <p className="text-muted-foreground mt-6">
+              Turn your vision into reality in just 3 simple steps
+            </p>
+          </div>
+        </Container>
+        <Container>
+          <div className="flex flex-col items-center justify-center py-10 md:py-20 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full divide-x-0 md:divide-x divide-y md:divide-y-0 divide-gray-900 first:border-l-2 lg:first:border-none first:border-gray-900">
+              {perks.map((perk) => (
+                <div
+                  key={perk.title}
+                  className="flex flex-col items-start px-4 md:px-6 lg:px-8 lg:py-6 py-4"
+                >
+                  <div className="flex items-center justify-center">
+                    <perk.icon className="w-8 h-8" />
+                  </div>
+                  <h3 className="text-lg font-medium mt-4">{perk.title}</h3>
+                  <p className="text-muted-foreground mt-2 text-start lg:text-start">
+                    {perk.info}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </Container>
+      </Wrapper>
+
+      {/* pricing */}
+      <Wrapper className="flex flex-col items-center justify-center py-12 relative">
+        <div className="hidden md:block absolute top-0 -right-1/3 w-72 h-72 bg-blue-500 rounded-full blur-[10rem] -z-10"></div>
+        <Container>
+          <div className="max-w-md mx-auto text-start md:text-center">
+            <SectionBadge title="Pricing" />
+            <h2 className="text-3xl lg:text-4xl font-semibold mt-6">
+              Unlock the right plan for your business
+            </h2>
+            <p className="text-muted-foreground mt-6">
+              Choose the best plan for your business and start building your
+              dream website today
+            </p>
+          </div>
+        </Container>
+        <Container className="flex items-center justify-center">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 w-full md:gap-8 py-10 md:py-20 flex-wrap max-w-4xl">
+            {pricingCards.map((card) => (
+              <Card
+                key={card.title}
+                className={cn(
+                  "flex flex-col w-full border-neutral-700",
+                  card.title === "Unlimited Saas" && "border-2 border-primary"
+                )}
+              >
+                <CardHeader className="border-b border-border">
+                  <span>{card.title}</span>
+                  <CardTitle
+                    className={cn(
+                      card.title !== "Unlimited Saas" && "text-muted-foreground"
+                    )}
+                  >
+                    {card.price}
+                  </CardTitle>
+                  <CardDescription>{card.description}</CardDescription>
+                </CardHeader>
+                <CardContent className="pt-6 space-y-3">
+                  {card.features.map((feature) => (
+                    <div key={feature} className="flex items-center gap-2">
+                      <Zap className="w-4 h-4 fill-primary text-primary" />
+                      <p>{feature}</p>
+                    </div>
+                  ))}
+                </CardContent>
+                <CardFooter className="mt-auto">
+                  <Link
+                    href="#"
+                    className={cn(
+                      "w-full text-center text-primary-foreground bg-primary p-2 rounded-md text-sm font-medium",
+                      card.title !== "Unlimited Saas" &&
+                        "!bg-foreground !text-background"
+                    )}
+                  >
+                    {card.buttonText}
+                  </Link>
+                </CardFooter>
+              </Card>
+            ))}
           </div>
         </Container>
       </Wrapper>
